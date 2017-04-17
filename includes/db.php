7cd2a73132da582a4c2f97e6ff8d4de6979060c4
rayGreycloud <rayGreycloud@gmail.com>
2017-04-17 07:18:37 -0700

    lecture 83 connect db

---
+++ includes/db.php
@@ -0,0 +1,23 @@
+<?php
+
+// Create array of connection parameters
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pwd'] = "";
+$db['db_name'] = "cms";
+
+// Create constants from array
+foreach($db as $key => $value) {
+  define(strtoupper($key), $value);
+}
+
+// Connect to db
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+
+if ($connection) {
+  echo "Database connected.";
+} else {
+  echo "Unable to connect to database.";
+}
+
+?>
