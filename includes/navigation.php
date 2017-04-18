2b596345c37abbea69838dd82ca673b1276c66af
rayGreycloud <rayGreycloud@gmail.com>
2017-04-18 14:25:54 -0700

    Add link to admin page

--- includes/navigation.php
+++ includes/navigation.php
@@ -28,15 +28,10 @@
 
 ?>
 
-        <!-- <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li> -->
+    <li>
+      <a href="admin">Admin</a>
+    </li>
+
       </ul>
     </div>
     <!-- /.navbar-collapse -->
