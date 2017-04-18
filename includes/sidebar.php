0471e2034633eec02d9503bf6c918ef373340c8a
rayGreycloud <rayGreycloud@gmail.com>
2017-04-18 08:45:02 -0700

    lecture 91 add search bar query

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,8 +3,26 @@
 <?php
 
 if(isset($_POST['submit'])) {
-  echo $search = $_POST['search'];
+  $search = $_POST['search'];
+
+  $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+
+  $search_query = mysqli_query($connection, $query);
+
+  if (!$search_query) {
+    die("QUERY FAILED: " . mysqli_error($connection));
+  }
+
+  $count = mysqli_num_rows($search_query);
+
+  if ($count == 0) {
+    echo "<h2>No results found.</h2>";
+  } else {
+    echo $count . " results found.";
+  }
 }
+
+
 ?>
 
   <!-- Blog Search Well -->
