e3274df217e700bc48f3aa943e8332894cd23369
rayGreycloud <rayGreycloud@gmail.com>
2017-04-18 11:44:51 -0700

    Remove dummy categories from sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -26,12 +26,10 @@ $select_categories_sidebar = mysqli_query($connection, $query);
 
 ?>
 
-
-
     <h4>Blog Categories</h4>
     <div class="row">
-        <div class="col-lg-6">
-          <ul class="list-unstyled">
+      <div class="col-lg-12">
+        <ul class="list-unstyled">
 
 <?php
 
@@ -42,22 +40,9 @@ while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
 }
 
 ?>
-          </ul>
-        </div>
-        <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
-          <ul class="list-unstyled">
-            <li><a href="#">Category Name</a>
-            </li>
-            <li><a href="#">Category Name</a>
-            </li>
-            <li><a href="#">Category Name</a>
-            </li>
-            <li><a href="#">Category Name</a>
-            </li>
-          </ul>
-        </div>
-        <!-- /.col-lg-6 -->
+        </ul>
+      </div>
+      <!-- /.col-lg-12 -->
     </div>
     <!-- /.row -->
   </div>
