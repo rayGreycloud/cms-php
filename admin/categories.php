b5a51bcb184979c454628613106342ba9130dcb3
rayGreycloud <rayGreycloud@gmail.com>
2017-04-19 10:25:45 -0700

    lecture 99 add logic to add new category to db

--- admin/categories.php
+++ admin/categories.php
@@ -25,7 +25,26 @@
               </li>
             </ol>
             <div class="col-xs-6">
-              <form action="">
+<?php
+if (isset($_POST['submit'])) {
+  $cat_title = $_POST['cat_title'];
+
+  if ($cat_title == "" || empty($cat_title)) {
+    echo "This field is required!";
+  } else {
+    $query = "INSERT INTO categories(cat_title) ";
+    $query .="VALUE('{$cat_title}') ";
+
+    $create_category_query = mysqli_query($connection, $query);
+
+    if (!$create_category_query) {
+      die('QUERY FAILED ' . mysqli_error($connection));
+    }
+  }
+}
+?>
+
+              <form action="" method="post">
                 <div class="form-group">
                   <label for="cat-title">ADD A NEW CATEGORY</label>
                   <input class="form-control" type="text" name="cat_title" placeholder="category title">
