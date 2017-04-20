fc23816d26cf8603697a427cc030cd7e0015104c
rayGreycloud <rayGreycloud@gmail.com>
2017-04-20 11:14:37 -0700

    Move find all categories query into functions

--- admin/categories.php
+++ admin/categories.php
@@ -41,24 +41,13 @@
               <hr>
 
 <?php
-
-if(isset($_GET['edit'])) {
-
-  $cat_id = $_GET['edit'];
-
-  include "includes/update_categories.php";
-}
-
+  if (isset($_GET['edit'])) {
+    $cat_id = $_GET['edit'];
+    include "includes/update_categories.php";
+  }
 ?>
             </div> <!-- /add category form-->
             <div class="col-xs-6">
-
-<?php
-
-  $query = "SELECT * FROM categories";
-  $select_categories = mysqli_query($connection, $query);
-
-?>
               <table class="table table-bordered table-hover">
                 <thead>
                   <tr>
@@ -68,19 +57,8 @@ if(isset($_GET['edit'])) {
                 </thead>
                 <tbody>
 
+<?php findAllCategories() ?>
 <?php
-
-  while($row = mysqli_fetch_assoc($select_categories)) {
-    $cat_id = $row['cat_id'];
-    $cat_title = $row['cat_title'];
-
-    echo "<tr>";
-    echo "<td>{$cat_id}</td>";
-    echo "<td>{$cat_title}</td>";
-    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-    echo "</tr>";
-  }
 // Delete query
   if (isset($_GET['delete'])) {
     $cat_id_to_delete = $_GET['delete'];
--- admin/functions.php
+++ admin/functions.php
@@ -3,7 +3,7 @@
 function insert_categories() {
 
   global $connection;
-  
+
   if (isset($_POST['submit'])) {
     $cat_title = $_POST['cat_title'];
 
@@ -19,6 +19,25 @@ function insert_categories() {
         die('QUERY FAILED ' . mysqli_error($connection));
       }
     }
+  }
+}
+
+function findAllCategories() {
+  global $connection;
+
+  $query = "SELECT * FROM categories";
+  $select_categories = mysqli_query($connection, $query);
+
+  while($row = mysqli_fetch_assoc($select_categories)) {
+    $cat_id = $row['cat_id'];
+    $cat_title = $row['cat_title'];
+
+    echo "<tr>";
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+    echo "</tr>";
   }
 }
 
