a8f2a9bc0cfb62da0288ce49623b1d92a54804a4
rayGreycloud <rayGreycloud@gmail.com>
2017-04-21 12:06:57 -0700

    lecture 112 add edit post form

---
+++ admin/includes/edit_post.php
@@ -0,0 +1,70 @@
+<?php
+  if (isset($_POST['edit_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['post_image']['name'];
+    $post_image_temp = $_FILES['post_image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_comment_count = 2;
+    $post_date = date('d-m-y');
+    $post_content = $_POST['post_content'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
+
+    $query .= "VALUES('{$post_category_id}', '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}') ";
+
+    $edit_post_query = mysqli_query($connection, $query);
+
+    confirmQuery($edit_post_query);
+  }
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+
+  <div class="form-group">
+    <label for="post_author">Author</label>
+    <input type="text" class="form-control" name="post_author">
+  </div>
+
+  <div class="form-group">
+    <label for="post_title">Title</label>
+    <input type="text" class="form-control" name="post_title">
+  </div>
+
+  <div class="form-group">
+    <label for="post_category_id">Category</label>
+    <input type="text" class="form-control" name="post_category_id">
+  </div>
+
+  <div class="form-group">
+    <label for="post_status">Status</label>
+    <input type="text" class="form-control" name="post_status">
+  </div>
+
+  <div class="form-group">
+    <label for="post_image">Image</label>
+    <input type="file" name="post_image">
+  </div>
+
+  <div class="form-group">
+    <label for="post_tags">Tags</label>
+    <input type="text" class="form-control" name="post_tags">
+  </div>
+
+  <div class="form-group">
+    <label for="post_content">Content</label>
+    <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+  </div>
+
+  <div class="form-group">
+    <input class="btn btn-primary" type="submit" name="create_post" value="Publish">
+  </div>
+
+</form>
