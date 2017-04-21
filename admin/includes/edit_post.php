60e5b730847ea1d698db7a0ce44b60f97eedb00f
rayGreycloud <rayGreycloud@gmail.com>
2017-04-21 13:35:21 -0700

    lecture 113 displaying data in edit post form

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,28 +1,25 @@
 <?php
-  if (isset($_POST['edit_post'])) {
+  if (isset($_GET['p_id'])) {
 
-    $post_author = $_POST['post_author'];
-    $post_title = $_POST['post_title'];
-    $post_category_id = $_POST['post_category_id'];
-    $post_status = $_POST['post_status'];
+    $post_id_to_edit = $_GET['p_id'];
 
-    $post_image = $_FILES['post_image']['name'];
-    $post_image_temp = $_FILES['post_image']['tmp_name'];
+    $query = "SELECT * FROM posts WHERE post_id = {$post_id_to_edit}";
+    $get_post_to_edit_query = mysqli_query($connection, $query);
 
-    $post_tags = $_POST['post_tags'];
-    $post_comment_count = 2;
-    $post_date = date('d-m-y');
-    $post_content = $_POST['post_content'];
+    while($row = mysqli_fetch_assoc($get_post_to_edit_query)) {
+      $post_id = $row['post_id'];
+      $post_author = $row['post_author'];
+      $post_title = $row['post_title'];
+      $post_category_id = $row['post_category_id'];
+      $post_status = $row['post_status'];
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+      $post_image = $row['post_image'];
+      $post_tags = $row['post_tags'];
+      $post_comment_count = $row['post_comment_count'];
+      $post_date = $row['post_date'];
+      $post_content = $row['post_content'];
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
-
-    $query .= "VALUES('{$post_category_id}', '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}') ";
-
-    $edit_post_query = mysqli_query($connection, $query);
-
-    confirmQuery($edit_post_query);
+    }
   }
 ?>
 
@@ -30,41 +27,41 @@
 
   <div class="form-group">
     <label for="post_author">Author</label>
-    <input type="text" class="form-control" name="post_author">
+    <input type="text" class="form-control" name="post_author" value="<?php echo $post_author; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_title">Title</label>
-    <input type="text" class="form-control" name="post_title">
+    <input type="text" class="form-control" name="post_title" value="<?php echo $post_title; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_category_id">Category</label>
-    <input type="text" class="form-control" name="post_category_id">
+    <input type="text" class="form-control" name="post_category_id" value="<?php echo $post_category_id; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_status">Status</label>
-    <input type="text" class="form-control" name="post_status">
+    <input type="text" class="form-control" name="post_status" value="<?php echo $post_status; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_image">Image</label>
-    <input type="file" name="post_image">
+    <input type="file" name="post_image" value="<?php echo $post_image; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_tags">Tags</label>
-    <input type="text" class="form-control" name="post_tags">
+    <input type="text" class="form-control" name="post_tags" value="<?php echo $post_tags; ?>">
   </div>
 
   <div class="form-group">
     <label for="post_content">Content</label>
-    <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+    <textarea class="form-control" name="post_content" id="" cols="30" rows="10" "><?php echo $post_content; ?></textarea>
   </div>
 
   <div class="form-group">
-    <input class="btn btn-primary" type="submit" name="create_post" value="Publish">
+    <input class="btn btn-primary" type="submit" name="create_post" value="Publish Update">
   </div>
 
 </form>
