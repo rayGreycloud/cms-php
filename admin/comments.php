7940b16176e47cad625b23e7adbd9bf7e72d4215
rayGreycloud <rayGreycloud@gmail.com>
2017-04-23 07:31:19 -0700

    Correct comments page title

--- admin/comments.php
+++ admin/comments.php
@@ -13,7 +13,7 @@
         <div class="row">
           <div class="col-lg-12">
             <h1 class="page-header">
-              Posts
+              Comments
               <small>Admin</small>
             </h1>
             <ol class="breadcrumb">
