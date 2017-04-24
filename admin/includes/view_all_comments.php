04915ac91059628b60c1dea6fbb824acf8f41e3f
rayGreycloud <rayGreycloud@gmail.com>
2017-04-24 10:15:43 -0700

    lecture 128 relating comments to posts

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -35,7 +35,13 @@
     echo "<td>$comment_email</td>";
     echo "<td>$comment_content</td>";
 
-    echo "<td>$comment_post_id</td>";
+    $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+    $select_post_id_query = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_post_id_query)) {
+      $comment_post_title = $row['post_title'];
+        echo "<td><a href='../post.php?p_id=$comment_post_id'> $comment_post_title</a></td>";
+    }
 
     echo "<td>$comment_date</td>";
     echo "<td>$comment_status</td>";
