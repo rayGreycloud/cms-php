02c44b90572d85fee353ef8997b7f5730d370a94
rayGreycloud <rayGreycloud@gmail.com>
2017-04-24 06:41:40 -0700

    lecture 125 add input fields to comment form

--- post.php
+++ post.php
@@ -56,15 +56,36 @@
 
 <!-- Blog Comments -->
 
+<?php
+
+  if (isset($_POST['create_comment'])) {
+    echo $_POST['comment_author'];
+  }
+
+?>
+
 <!-- Comments Form -->
 <div class="well">
-    <h4>Leave a Comment:</h4>
-    <form role="form">
-        <div class="form-group">
-            <textarea class="form-control" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <h4>Leave a Comment:</h4>
+  <form action="" method="POST" role="form">
+
+    <div class="form-group">
+      <label for="author">Author</label>
+      <input class="form-control" type="text" name="comment_author"></input>
+    </div>
+
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input class="form-control" type="email" name="comment_email"></input>
+    </div>
+
+    <div class="form-group">
+      <label for="comment">Comment</label>
+      <textarea class="form-control" rows="3" name="comment_content"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
+  </form>
 </div>
 
 <hr>
