4ebca58256d77c558cd70d239395eef2f0359f90
rayGreycloud <rayGreycloud@gmail.com>
2017-04-24 20:40:02 -0700

    Revise posts query to display published only

--- index.php
+++ index.php
@@ -17,16 +17,20 @@
         </h1>
 
 <?php
-  $query = "SELECT * FROM posts";
+  $query = "SELECT * FROM posts WHERE post_status = 'published' ";
   $select_all_posts_query = mysqli_query($connection, $query);
 
-  while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-    $post_id = $row['post_id'];
-    $post_title = $row['post_title'];
-    $post_author = $row['post_author'];
-    $post_date = $row['post_date'];
-    $post_image = $row['post_image'];
-    $post_content = substr($row['post_content'],0,222);
+  if (mysqli_num_rows($select_all_posts_query) == 0) {
+    echo "<h2 class='text-center'>No posts found.</h2>";
+  } else {
+
+    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+      $post_id = $row['post_id'];
+      $post_title = $row['post_title'];
+      $post_author = $row['post_author'];
+      $post_date = $row['post_date'];
+      $post_image = $row['post_image'];
+      $post_content = substr($row['post_content'],0,222);
 
 ?>
 
@@ -46,7 +50,10 @@
 
         <hr>
 
-<?php } ?>
+<?php
+      }
+    }
+?>
 
         <!-- Pager -->
         <ul class="pager">
