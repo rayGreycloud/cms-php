0330b5ab530141e1963350b05457decf87632dbe
rayGreycloud <rayGreycloud@gmail.com>
2017-04-26 07:09:35 -0700

    Add edit link to admin users display

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -9,6 +9,7 @@
                   <th>Email</th>
                   <th>Image</th>
                   <th>Role</th>
+                  <th>Edit</th>
                   <th>Delete</th>
                 </tr>
               </thead>
@@ -35,7 +36,7 @@
     echo "<td>$user_email</td>";
     echo "<td>$user_image</td>";
     echo "<td>$user_role</td>";
-
+    echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
     echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
     echo "<tr>";
 
