c7b5d08eec8d2c2aa5e0a63027a72b0d55248f34
rayGreycloud <rayGreycloud@gmail.com>
2017-04-28 09:56:32 -0700

    Add query to update profile

--- admin/profile.php
+++ admin/profile.php
@@ -25,6 +25,49 @@
 
 ?>
 
+<?php
+
+  if (isset($_POST['edit_user'])) {
+
+    $username = $_POST['username'];
+    $user_password = $_POST['user_password'];
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_email = $_POST['user_email'];
+    $user_image = $_FILES['user_image']['name'];
+    $user_image_temp = $_FILES['user_image']['tmp_name'];
+    $user_role = $_POST['user_role'];
+
+    move_uploaded_file($user_image_temp, "../images/$user_image");
+
+    if (empty($user_image)) {
+      $query = "SELECT * FROM users WHERE user_id = {$user_id_to_edit}";
+      $select_image = mysqli_query($connection, $query);
+
+      while($row = mysqli_fetch_array($select_image)) {
+        $user_image = $row['user_image'];
+      }
+    }
+
+    $query = "UPDATE users SET ";
+    $query .= "username = '{$username}', ";
+    $query .= "user_password = '{$user_password }', ";
+    $query .= "user_firstname = '{$user_firstname }', ";
+    $query .= "user_lastname = '{$user_lastname }', ";
+    $query .= "user_email = '{$user_email }', ";
+    $query .= "user_image = '{$user_image}', ";
+    $query .= "user_role = '{$user_role}' ";
+    $query .= "WHERE user_id = {$user_id} ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+
+    confirmQuery($edit_user_query);
+
+    header("Location: index.php");
+  }
+
+?>
+
 
   <div id="wrapper">
 
