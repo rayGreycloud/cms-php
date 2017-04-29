33671c782ea56cc293403a15919d01df9b84e03e
rayGreycloud <rayGreycloud@gmail.com>
2017-04-29 07:03:13 -0700

    Add TinyMCE text editor

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -35,6 +35,11 @@
   <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+  <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+
+  <script src="./js/scripts.js"></script>
+
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
