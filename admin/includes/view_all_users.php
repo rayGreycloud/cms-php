f922db716bcd0312289ab274ed53cf566089254a
rayGreycloud <rayGreycloud@gmail.com>
2017-05-01 13:55:26 -0700

    Display user image in view_all_users and uppercase user role

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -34,8 +34,8 @@
     echo "<td>$user_firstname</td>";
     echo "<td>$user_lastname</td>";
     echo "<td>$user_email</td>";
-    echo "<td>$user_image</td>";
-    echo "<td>$user_role</td>";
+    echo "<td><img src='../images/$user_image'></td>";
+    echo "<td>" . ucfirst($user_role) . "</td>";
     echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
     echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
     echo "<tr>";
