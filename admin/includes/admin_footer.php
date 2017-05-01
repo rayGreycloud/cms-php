d8b6c536c33dec1700930edf15d23ff702693bff
rayGreycloud <rayGreycloud@gmail.com>
2017-05-01 10:53:41 -0700

    Move custom js script tag from header to footer

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -4,6 +4,9 @@
   <!-- jQuery -->
   <script src="js/jquery.js"></script>
 
+  <!-- custom js -->
+  <script src="./js/scripts.js"></script>
+
   <!-- Bootstrap Core JavaScript -->
   <script src="js/bootstrap.min.js"></script>
 
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -37,7 +37,6 @@
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 <?php include "./../config/tinymce.php"; ?>
-<script src="./js/scripts.js"></script>
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
