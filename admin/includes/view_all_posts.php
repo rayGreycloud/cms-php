8857ee2432eafe4fa2b196f0ca71be703fa6a7d5
rayGreycloud <rayGreycloud@gmail.com>
2017-05-01 09:46:31 -0700

    Add query for bulk option to update post status

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,13 +1,37 @@
+<?php
 
-          <form action="" method="post" class="">
+  if (isset($_POST['checkBoxArray'])) {
+
+    foreach ($_POST['checkBoxArray'] as $checkedPost_Id) {
+      $bulk_options = $_POST['bulk_options'];
+
+      $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$checkedPost_Id} ";
+      $update_to_post_status = mysqli_query($connection, $query);
+
+      confirmQuery($update_to_post_status);
+
+      // switch($bulk_options) {
+        // case 'draft':
+        //   $query = "UPDATE * FROM posts SET post_status = 'published' WHERE post_id = $checkedPost_Id";
+        //   break;
+      // }
+    }
+
+  }
+
+
+ ?>
+
+
+          <form action="" method="post" class="form-group">
             <table class="table table-bordered table-hover">
 
               <div id="bulkOptionContainer" class="col-xs-4">
-                <select class="form-control" name="" id="">
+                <select class="form-control" name="bulk_options" id="">
                   <option value="">Select Options</option>
-                  <option value="">Publish</option>
-                  <option value="">Draft</option>
-                  <option value="">Delete</option>
+                  <option value="published">Publish</option>
+                  <option value="draft">Draft</option>
+                  <option value="delete">Delete</option>
                 </select>
               </div>
               <div class="col-xs-4">
@@ -51,7 +75,7 @@
 
     echo "<tr>";
 ?>
-    <td><input class="checkBoxes" type="checkbox" name="checkBoxArray[]" value="<?php echo $post_id ?>"></td>";
+    <td><input class="checkBoxes" type="checkbox" name="checkBoxArray[]" value="<?php echo $post_id; ?>"></td>
 
 <?php
     echo "<td>$post_id</td>";
