e54de48b2ae9270ea08bf6e8c60cc1d8ff1a7d26
rayGreycloud <rayGreycloud@gmail.com>
2017-05-02 10:32:09 -0700

    Add width attribute to user image in user dashboard

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -34,7 +34,7 @@
     echo "<td>$user_firstname</td>";
     echo "<td>$user_lastname</td>";
     echo "<td>$user_email</td>";
-    echo "<td><img src='../images/$user_image'></td>";
+    echo "<td><img width='50rem' src='../images/$user_image'></td>";
     echo "<td>" . ucfirst($user_role) . "</td>";
     echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
     echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
