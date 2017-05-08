cd5918b5a02877069aa3caa66fb8a3033892dc59
rayGreycloud <rayGreycloud@gmail.com>
2017-05-08 11:16:47 -0700

    Add link to post author for author_posts

--- index.php
+++ index.php
@@ -36,18 +36,18 @@
 
         <!-- Blog Post Template -->
         <h2>
-          <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+          <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
         </h2>
         <p class="lead">
-          by <a href="index.php"><?php echo $post_author ?></a>
+          by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
         </p>
-        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
         <hr>
         <a href="post.php?p_id=<?php echo $post_id; ?>">
-          <img class="img-responsive" src="./images/<?php echo $post_image ?>" alt="">
+          <img class="img-responsive" src="./images/<?php echo $post_image; ?>" alt="">
         </a>
         <hr>
-        <p><?php echo $post_content ?></p>
+        <p><?php echo $post_content ;?></p>
         <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
         <hr>
