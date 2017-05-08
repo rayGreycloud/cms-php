2a6289d4e2f5c978b118994eb53b0ea1fc643c88
rayGreycloud <rayGreycloud@gmail.com>
2017-05-08 07:59:08 -0700

    Refactor login to use encrypted passwords

--- includes/login.php
+++ includes/login.php
@@ -21,14 +21,15 @@
 
       $db_user_id = $row['user_id'];
       $db_username = $row['username'];
-      $db_user_password = $row['user_password'];
+      $db_hashed_pwd = $row['user_password'];
       $db_user_firstname = $row['user_firstname'];
       $db_user_lastname = $row['user_lastname'];
       $db_user_role = $row['user_role'];
-      
+      $salt = $row['randSalt'];
     }
 
-    if ($password === $db_user_password) {
+    $user_hashed_pwd = crypt($password, $salt);
+    if (hash_equals($user_hashed_pwd, $db_hashed_pwd)) {
 
       $_SESSION['username'] = $db_username;
       $_SESSION['firstname'] = $db_user_firstname;
