480d5f16659ed04812715bbfd803a231f4a4930f
rayGreycloud <rayGreycloud@gmail.com>
2017-05-10 14:10:31 -0700

    Add tinymce config file with inline option

---
+++ config/tinymce.php
@@ -0,0 +1,7 @@
+<?php
+
+echo
+  "<script src='https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=2snaotiighduvnaeny4859ninfq920cgahzpql13bxkjn23d'></script>" ;
+
+  "<script>tinymce.init({selector: 'textarea', inline: true });</script>" ;
+?>
