0547d89ad02210361d9e3fdd46539cec3e69098e
rayGreycloud <rayGreycloud@gmail.com>
2017-05-12 13:21:44 -0700

    Move post comments above add comment form

--- post.php
+++ post.php
@@ -66,7 +66,40 @@ if (isset($_GET['p_id'])) {
  ?>
 
 <!-- Blog Comments -->
+<!-- Posted Comments -->
+<?php
+$comment_post_id = $_GET['p_id'];
+$query = "SELECT * FROM comments WHERE comment_post_id = $comment_post_id ";
+$query .= "AND comment_status = 'approved' ";
+$query .= "ORDER BY comment_id DESC ";
+$select_comment_query = mysqli_query($connection, $query);
+confirmQuery($select_comment_query);
 
+while ($row = mysqli_fetch_array($select_comment_query)) {
+  $comment_date = $row['comment_date'];
+  $comment_content = $row['comment_content'];
+  $comment_author = $row['comment_author'];
+?>
+
+<!-- Comment template -->
+<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="http://placehold.it/64x64" alt="">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">
+      <?php echo $comment_author; ?>
+      <small><?php echo $comment_date; ?></small>
+    </h4>
+    <?php echo $comment_content; ?>
+  </div>
+</div>
+
+<hr>
+<?php } ?>
+
+
+<!-- Comment form -->
 <?php
 
 if (isset($_POST['create_comment'])) {
@@ -85,21 +118,17 @@ if (isset($_POST['create_comment'])) {
     $create_comment_query = mysqli_query($connection, $query);
 
     confirmQuery($create_comment_query);
-
 /*    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
     $query .= "WHERE post_id = $comment_post_id ";
     $increment_comment_count_query = mysqli_query($connection, $query);
     confirmQuery($increment_comment_count_query);
 */
-
   } else {
     echo "<script>alert('All fields are required')</script>";
   }
 }
-
 ?>
 
-<!-- Comments Form -->
 <div class="well">
   <h4>Leave a Comment:</h4>
   <form action="" method="POST" role="form">
@@ -123,39 +152,6 @@ if (isset($_POST['create_comment'])) {
   </form>
 </div>
 
-<hr>
-
-<!-- Posted Comments -->
-<?php
-$comment_post_id = $_GET['p_id'];
-$query = "SELECT * FROM comments WHERE comment_post_id = $comment_post_id ";
-$query .= "AND comment_status = 'approved' ";
-$query .= "ORDER BY comment_id DESC ";
-$select_comment_query = mysqli_query($connection, $query);
-confirmQuery($select_comment_query);
-
-while ($row = mysqli_fetch_array($select_comment_query)) {
-  $comment_date = $row['comment_date'];
-  $comment_content = $row['comment_content'];
-  $comment_author = $row['comment_author'];
-
-?>
-
-<!-- Comment -->
-<div class="media">
-  <a class="pull-left" href="#">
-    <img class="media-object" src="http://placehold.it/64x64" alt="">
-  </a>
-  <div class="media-body">
-    <h4 class="media-heading">
-      <?php echo $comment_author; ?>
-      <small><?php echo $comment_date; ?></small>
-    </h4>
-    <?php echo $comment_content; ?>
-  </div>
-</div>
-
-<?php } ?>
 
         <!-- Pager -->
         <ul class="pager">
