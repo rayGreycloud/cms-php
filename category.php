1b45ee8454cdb6a8ccefd3bcaae2cfee641f6caf
rayGreycloud <rayGreycloud@gmail.com>
2017-05-17 14:25:42 -0700

    Add href to read more button

--- category.php
+++ category.php
@@ -75,7 +75,7 @@ if (isset($_GET['category'])) {
         <img class="img-responsive" src="./images/user/<?php echo $post_image ?>" alt="">
         <hr>
         <p><?php echo $post_content ?></p>
-        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
         <hr>
 
