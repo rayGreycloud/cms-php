3aff553be2faf9b5561e73dbb2e519e8fde0a525
rayGreycloud <rayGreycloud@gmail.com>
2017-05-20 07:30:47 -0700

    Refactor query for edit category form with prepared statements

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -5,21 +5,30 @@
 <?php
 
   if (isset($_GET['edit'])) {
+
+    // Make add category form disappear
+    // Set visiblity to none on add-category__form
+
     $cat_id_to_update = escape($_GET['edit']);
 
-    $query = "SELECT * FROM categories WHERE cat_id = {$cat_id_to_update}";
-    $update_category_query = mysqli_query($connection, $query);
+    $query = "SELECT cat_id, cat_title FROM categories WHERE cat_id = ? ";
 
-    while($row = mysqli_fetch_assoc($update_category_query)) {
-    $cat_id = $row['cat_id'];
-    $cat_title = $row['cat_title'];
+    $stmt = mysqli_prepare($connection, $query);
+    mysqli_stmt_bind_param($stmt, "i", $cat_id_to_update);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $cat_id, $cat_title);
+    mysqli_stmt_store_result($stmt);
 
-  }
+    while(mysqli_stmt_fetch($stmt)):
+
  ?>
 
 <input value="<?php if(isset($cat_title)){echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
 
-<?php } ?>
+<?php
+    endwhile;
+  }
+ ?>
 
 <?php
   if (isset($_POST['update_category'])) {
@@ -30,6 +39,7 @@
     mysqli_stmt_bind_param($stmt, "si", $cat_title_to_update, $cat_id_to_update);
     mysqli_stmt_execute($stmt);
 
+    // Set visiblity to visible on add-category__form
     header("Location: categories.php");
   }
 ?>
