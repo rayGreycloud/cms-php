0b67661d48a5279cfed8934222509848866e556f
rayGreycloud <rayGreycloud@gmail.com>
2017-05-31 14:06:48 -0700

    Comment out widget but retain as placeholder

--- includes/widget.php
+++ includes/widget.php
@@ -1,4 +1,4 @@
-<div class="well">
+<!-- <div class="well">
   <h4>Spicy Side Widget</h4>
   <p>Spicy jalapeno beef ribs exercitation tail voluptate velit cupim. Dolore deserunt et ullamco rump prosciutto ground round tempor pastrami jerky kielbasa.</p>
-</div>
+</div> -->
