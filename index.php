bffaaf4065759b91641bee297e8068e3e5b5d9ed
rayGreycloud <rayGreycloud@gmail.com>
2017-05-31 16:07:02 -0700

    Update link on author name to author_posts

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if ($posts_count == 0) {
           <a href="./post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
         </h2>
         <p class="lead">
-          by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+          by <a href="author_posts.php?author=<?php echo $post_author; ?>"><?php echo $post_author; ?></a>
         </p>
         <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
         <hr>
