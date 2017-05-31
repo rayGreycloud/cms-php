171a9315794a2381201596bdc6995ce07a7b9a03
rayGreycloud <rayGreycloud@gmail.com>
2017-05-31 16:18:29 -0700

    Add read more button to post

--- author_posts.php
+++ author_posts.php
@@ -69,7 +69,7 @@ if (isset($_GET['author'])) {
         <img class="img-responsive" src="./images/user/<?php echo $post_image ?>" alt="">
         <hr>
         <p><?php echo $post_content ?></p>
-
+        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
         <hr>
 
 <?php
