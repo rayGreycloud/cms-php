2f62a2c020e6c233be52ce98c5904cec8535c68d
rayGreycloud <rayGreycloud@gmail.com>
2017-06-01 09:02:49 -0700

    Add pagination to search results

--- search.php
+++ search.php
@@ -11,74 +11,96 @@
 
       <!-- Blog Entries Column -->
       <div class="col-md-8">
-        <h1 class="page-header">
-          Search
-          <small>Results</small>
-        </h1>
 
 <?php
+
 if(isset($_POST['submit'])) {
   $search = escape($_POST['search']);
 
-  $query = "SELECT post_id, post_title, post_author, post_date, post_image, post_content ";
-  $query .= "FROM posts WHERE post_tags LIKE ? ";
-  $search_term = $search . '%';
+  echo "<h1 class='page-header'>Search <small> {$search}</small></h1>";
 
+  if (isset($_GET['page'])) {
+    $page_requested = $_GET['page'];
+  } else {
+    $page_requested = "";
+  }
+
+  if ($page_requested == "" || $page_requested == 1) {
+    $top_page = 0;
+  } else {
+    $top_page = ($page_requested * 5) - 5;
+  }
+  // Get post count for pagination calculation
+  $query = "SELECT * FROM posts WHERE post_tags LIKE '{$search}%' ";
+  $posts_count_query = mysqli_query($connection, $query);
+  confirmQuery($posts_count_query);
+  $posts_count = mysqli_num_rows($posts_count_query);
+  $page_count = ceil($posts_count / 5);
+  echo "posts: " . $posts_count;
+  echo "pages: " . $page_count;
+
+  if ($posts_count == 0) {
+    echo "<h3 class='text-center bg-primary'>No posts found.</h3>";
+  } else {
+
+  $query = "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_comment_count ";
+  $query .= "FROM posts WHERE post_tags LIKE ? ";
+  $query .= "LIMIT ?, 5";
+  $search_term = $search . '%';
   $stmt = mysqli_prepare($connection, $query);
 
-  mysqli_stmt_bind_param($stmt, "s", $search_term);
-
+  mysqli_stmt_bind_param($stmt, "si", $search_term, $top_page);
   mysqli_stmt_execute($stmt);
-
-  mysqli_stmt_bind_result($stmt, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
-
+  confirmQuery($stmt);
+  mysqli_stmt_bind_result($stmt, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content, $post_comment_count);
   mysqli_stmt_store_result($stmt);
 
-  if (mysqli_stmt_num_rows($stmt) == 0) {
-      echo "<h2 class='text-center bg-primary'>No posts found.</h2>";
-  } else {
-    while(mysqli_stmt_fetch($stmt)):
+  while(mysqli_stmt_fetch($stmt)):
+  $post_content = substr($post_content,0,222);
+  ?>
 
-    $post_content = substr($post_content,0,222);
+        <!-- Blog Post Template -->
+        <h2>
+          <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+        </h2>
+        <p class="lead">
+          by <a href="author_posts.php?author=<?php echo $post_author; ?>"><?php echo $post_author ?></a>
+        </p>
+        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+        <hr>
+        <img class="img-responsive" src="./images/user/<?php echo $post_image ?>" alt="">
+        <hr>
+        <p><?php echo $post_content ?></p>
+        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <div class="pull-right"><i class="fa fa-comments fa-2x"></i> <huge><?php echo $post_comment_count ?></huge></div>
 
- ?>
-
-      <!-- Blog Post Template -->
-      <h2>
-        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-      </h2>
-      <p class="lead">
-        by <a href="index.php"><?php echo $post_author ?></a>
-      </p>
-      <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-      <hr>
-      <img class="img-responsive" src="./images/user/<?php echo $post_image ?>" alt="">
-      <hr>
-      <p><?php echo $post_content ?></p>
-      <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-      <hr>
+        <hr>
 
 <?php
     endwhile;
-    mysqli_stmt_close($stmt);
+    // mysqli_stmt_close($stmt);
   }
 }
  ?>
 
+ <!-- Pager -->
+ <ul class="pagination">
 
-      <!-- Pager -->
-      <ul class="pager">
-          <li class="previous">
-              <a href="#">&larr; Older</a>
-          </li>
-          <li class="next">
-              <a href="#">Newer &rarr;</a>
-          </li>
-      </ul>
+<?php
+  for ($i = 1; $i <= $page_count; $i++) {
+    $active_class = '';
 
-    </div>
+    if ($i == $page_requested) {
+    $active_class = 'active';
+    }
 
+    echo "<li class='{$active_class}'><a href='search.php?search={$search}%&page={$i}'>{$i}</a></li>";
+  }
+?>
+
+ </ul>
+
+      </div> <!-- /.col-md-8 -->
 
       <!-- Blog Sidebar Widgets Column -->
 <?php include "./includes/sidebar.php"; ?>
